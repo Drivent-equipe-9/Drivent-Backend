75d41b044d839d45e51ae6216cf737feb825908d
luis garbelotti <luisgsgarbelotti@gmail.com>
2022-06-09 04:20:55 -0300

    test: add POST requisition tests
    This add tests to create a new ticket via POST requisition when no token is given, a invalid token, invalid body and all data valid (token and body)

--- tests/integration/ticket.test.ts
+++ tests/integration/ticket.test.ts
@@ -5,6 +5,7 @@ import supertest from 'supertest';
 import * as jwt from 'jsonwebtoken';
 import { createEnrollmentWithAddress, createEvent, createTicket, createUser } from '../factories';
 import { cleanDb, generateValidToken } from '../helpers';
+import { prisma } from '@/config';
 
 beforeAll(async () => {
   await init();
@@ -13,7 +14,7 @@ beforeAll(async () => {
 
 const server = supertest(app);
 
-describe('/ticket/:id', () => {
+describe('GET /ticket/:id', () => {
   it('should respond with status 401 if not given enrollment id', async () => {
     const response = await server.get(`/ticket`);
 
@@ -69,6 +70,73 @@ describe('/ticket/:id', () => {
 
       expect(response.status).toBe(httpStatus.OK);
       expect(response.body).toEqual(ticket);
+    });
+  });
+});
+
+describe('POST /ticket', () => {
+  it('should respond with status 401 if no token is given', async () => {
+    const response = await server.post('/ticket');
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 401 if given token is not valid', async () => {
+    const token = faker.lorem.word();
+
+    const response = await server.post('/ticket').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 401 if there is no session for given token', async () => {
+    const userWithoutSession = await createUser();
+    const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+    const response = await server.post('/ticket').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 400 when body is not present', async () => {
+    const user = await createUser();
+    const token = await generateValidToken(user);
+
+    const response = await server.post('/ticket').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.BAD_REQUEST);
+  });
+
+  it('should respond with status 400 when body is not valid', async () => {
+    const user = await createUser();
+    const token = await generateValidToken(user);
+    const body = { [faker.lorem.word()]: faker.lorem.word() };
+
+    const response = await server.post('/ticket').set('Authorization', `Bearer ${token}`).send(body);
+
+    expect(response.status).toBe(httpStatus.BAD_REQUEST);
+  });
+
+  describe('when body is valid', () => {
+    it('should respond with status 201 and create a ticket if there is not any', async () => {
+      const user = await createUser();
+      const event = await createEvent();
+      const enrollment = await createEnrollmentWithAddress(user);
+      const token = await generateValidToken(user);
+      const generateValidBody = () => ({
+        eventId: event.id,
+        enrollmentId: enrollment.id,
+        isOnline: true,
+        withAccommodation: false,
+        totalPrice: 100,
+      });
+      const body = generateValidBody();
+
+      const response = await server.post('/ticket').set('Authorization', `Bearer ${token}`).send(body);
+
+      expect(response.status).toBe(httpStatus.CREATED);
+      const ticket = await prisma.ticket.findFirst({ where: { enrollmentId: enrollment.id } });
+      expect(ticket).toBeDefined();
     });
   });
 });
