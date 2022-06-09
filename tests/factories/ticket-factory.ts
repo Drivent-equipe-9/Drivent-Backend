eb28734e6a9347d8d1fff6089d60184c2b29f8a6
luis garbelotti <luisgsgarbelotti@gmail.com>
2022-06-09 04:13:50 -0300

    test: remove not necessary imports

--- tests/factories/ticket-factory.ts
+++ tests/factories/ticket-factory.ts
@@ -1,8 +1,3 @@
-import faker from '@faker-js/faker';
-import { generateCPF, getStates } from '@brazilian-utils/brazilian-utils';
-import { User } from '@prisma/client';
-
-import { createUser } from './users-factory';
 import { prisma } from '@/config';
 
 export async function createTicket(enrollmentId: number, eventId: number) {
